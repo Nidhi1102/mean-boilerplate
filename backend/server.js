13c3bef0209b66256bcbfe5c7fe0ffeaa9e9a7d5
rafaMontoya <rafamontoya@gmail.com>
2020-09-08 00:50:42 +0200

    rename from server to backend

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const app = express();
 
 
 //app.use(express.static('./angular/dist'));
-//OK app.use(express.static(__dirname + '/../angular/dist'));
+//app.use(express.static(__dirname + '/../angular/dist'));
 app.use(express.static('angular/dist/meanjs20'));
 
 app.get('/*', function(req, res) {
     console.log('El valor de __dirname es ', __dirname);
     //res.sendFile('index.html', { root: 'angular/dist/' });
     //res.sendFile('./angular/dist/index.html');
-    //OK res.sendFile(path.join(__dirname + '/../angular/dist/meanjs20/index.html'));
+    //res.sendFile(path.join(__dirname + '/../angular/dist/meanjs20/index.html'));
     res.sendFile(path.resolve(__dirname + 'angular/dist/meanjs20/index.html'));
 });
 
